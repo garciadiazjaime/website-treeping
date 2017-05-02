fbec12c454c66d36430fade85249ddf0a97a25be
alex-romanov <alejandro@mintitmedia.com>
2017-05-02 13:46:55 -0700

    defined create story form input values

--- src/shared/components/sections/story/form.jsx
+++ src/shared/components/sections/story/form.jsx
@@ -1,4 +1,4 @@
-/* eslint max-len: [2, 500, 4] */
+/* eslint max-len: [2, 10000, 4] */
 /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
@@ -6,6 +6,7 @@ import _ from 'lodash';
 import TextField from 'material-ui/TextField';
 import DatePicker from 'material-ui/DatePicker';
 import RaisedButton from 'material-ui/RaisedButton';
+// import Upload from 'material-ui-upload/Upload';
 import { ContentClear } from 'material-ui/svg-icons';
 import LinearProgress from 'material-ui/LinearProgress';
 
@@ -75,11 +76,19 @@ export default class ActivityForm extends Component {
       <Link to="/" className="pull-right">
         <ContentClear />
       </Link>
-      <TextField name="name" floatingLabelText="Name" floatingLabelFixed fullWidth onChange={this.handleInputChange} errorText={!valid.name && touch.name ? this.invalidText : null} defaultValue={data.name} />
+      <TextField name="title" floatingLabelText="Title" floatingLabelFixed fullWidth onChange={this.handleInputChange} errorText={!valid.name && touch.name ? this.invalidText : null} defaultValue={data.name} />
       <br />
-      <TextField name="description" floatingLabelText="Description" floatingLabelFixed multiLine rows={4} fullWidth onChange={this.handleInputChange} errorText={!valid.description && touch.description ? this.invalidText : null} defaultValue={data.description} />
+      <h2>Upload Image</h2>
+      {/* <Upload onFileLoad={this.onFileLoad} />
+    I tried to install this module but I failed. Please teach me how to do it.
+      */}
       <br />
       <DatePicker name="date" floatingLabelText="Date" fullWidth onChange={this.handleInputChange} autoOk defaultDate={new Date(data.date)} />
+      {/*
+          Adjascent panels should be an ordered list of 0 to 4 panel id's. Ordered beacuse each of these panles will be assigned to a navigational position (top, right, bottom, left).
+
+          On author mode, each panel will have up to 4 create-panel-buttons (for each postion), triggering this button should assign the new panel su it's position on the current panel, and the opposite position in the new panel (Ex: if I create new-panel to the right of current-panel; current-panel's right position will have the id for new-panel and new-panel's left postion will have the id of current-panel. Also new-panel will only have 3 create-panel buttons as one of it's positions will be occupied by the old current-panel).
+      */}
       <br />
       <RaisedButton label="Save" primary fullWidth onTouchTap={this.handleSubmit} />
       <br />
