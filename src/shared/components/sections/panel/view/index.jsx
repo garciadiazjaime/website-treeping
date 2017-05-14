e8f84fe3fffc1065df05b59fe8053676a69b847c
alex-romanov <alejandro@mintitmedia.com>
2017-05-14 11:07:49 +0300

    basic panel layout
    Navigation and creator tools

--- src/shared/components/sections/panel/view/index.jsx
+++ src/shared/components/sections/panel/view/index.jsx
@@ -1,13 +1,11 @@
 /* eslint max-len: [2, 500, 4] */
 /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
-
-// const style = require('./style.scss');
-
-import { getPanels } from '../../../../actions/panel/view';
+// import { getPanels } from '../../../../actions/panel/view';
 import PanelViewContainer from '../../../../containers/panel/view';
-import NavButton from '../../../layout/navigation/panel-navigation-button';
+
+const style = require('./style.scss');
 
 class PanelView extends Component {
 
@@ -18,25 +16,37 @@ class PanelView extends Component {
     };
   }
 
-  componentDidMount() {
+  /* componentDidMount() {
     const { params } = this.props;
     const { dispatch } = this.props;
     dispatch(getPanels(params.userId));
-  }
+  }*/
 
   render() {
-    return (<div>
-      <Link to="/panel/add" className="pull-right">
-        Hola
-      </Link>
-      <NavButton />
+    return (<div className={style.comicContainer}>
+      <div className={style.panel}>
+        <div className={style.panelNavTools}>
+          <Link to="/panel/top" className={style.arrowTop} />
+          <Link to="/panel/bottom" className={style.arrowBottom} />
+          <Link to="/panel/left" className={style.arrowLeft} />
+          <Link to="/panel/right" className={style.arrowRight} />
+        </div>
+        <div className={style.creatorToolContainer}>
+          <Link to="/panel/delete" className={style.creatorTool}>
+            <span className={style.editIcon} />
+          </Link>
+          <Link to="/panel/edit" className={style.creatorTool}>
+            <span className={style.deleteIcon} />
+          </Link>
+        </div>
+      </div>
     </div>);
   }
 }
 
 PanelView.propTypes = {
-  params: PropTypes.shape({}).isRequired,
-  dispatch: PropTypes.func.isRequired,
+  /* params: PropTypes.shape({}).isRequired,
+  dispatch: PropTypes.func.isRequired,*/
 };
 
 export default PanelViewContainer(PanelView);
