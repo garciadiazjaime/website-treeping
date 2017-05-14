18295f3849bc6e048c46ce7f34b829a6529509d5
alex-romanov <alejandro@mintitmedia.com>
2017-05-14 12:23:15 +0300

    Navigation and creator tools

--- src/shared/components/sections/panel/view/index.jsx
+++ src/shared/components/sections/panel/view/index.jsx
@@ -25,11 +25,19 @@ class PanelView extends Component {
   render() {
     return (<div className={style.comicContainer}>
       <div className={style.panel}>
-        <div className={style.panelTools}>
+        <div className={style.panelNavTools}>
           <Link to="/panel/top" className={style.arrowTop} />
           <Link to="/panel/bottom" className={style.arrowBottom} />
           <Link to="/panel/left" className={style.arrowLeft} />
           <Link to="/panel/right" className={style.arrowRight} />
+        </div>
+        <div className={style.creatorToolContainer}>
+          <Link to="/panel/delete" className={style.creatorTool}>
+            <span className={style.editIcon} />
+          </Link>
+          <Link to="/panel/edit" className={style.creatorTool}>
+            <span className={style.deleteIcon} />
+          </Link>
         </div>
       </div>
     </div>);
