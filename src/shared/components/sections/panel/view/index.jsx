5d6b515aebb399845e77f7493154906d10b2bb58
alex-romanov <alejandro@mintitmedia.com>
2017-06-15 14:17:42 +0200

    flow complete fixing css mechanics

--- src/shared/components/sections/panel/view/index.jsx
+++ src/shared/components/sections/panel/view/index.jsx
@@ -1,9 +1,9 @@
 
 /* eslint max-len: [2, 500, 4] */
 /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
-
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
+
 // import { getPanels } from '../../../../actions/panel/view';
 import PanelViewContainer from '../../../../containers/panel/view';
 
@@ -18,21 +18,26 @@ class PanelView extends Component {
     };
   }
 
-  /* componentDidMount() {
-    const { params } = this.props;
-    const { dispatch } = this.props;
-    dispatch(getPanels(params.userId));
-  }*/
+  getNavPanel(dir) {
+    if (dir) {
+      return [this.props.topUrl, style.arrowTop];
+    }
+    return ['/view/add', style.newPanelTop];
+  }
 
   render() {
+    const navPanelTop = this.getNavPanel(this.props.topUrl);
+    const navPanelUrlTop = navPanelTop[0];
+    const navPanelIconTop = navPanelTop[1];
+
     return (<div className={style.comicContainer}>
       <div className={style.panel}>
         <img src="/images/the-leap.png" alt="The Leap" />
         <div className={style.panelNavTools}>
-          <Link to="/panel/top" className={style.newPanelTop} />
-          <Link to="/panel/bottom" className={style.newPanelBottom} />
-          <Link to="/panel/left" className={style.newPanelLeft} />
-          <Link to="/panel/right" className={style.newPanelRight} />
+          <Link to={navPanelUrlTop} className={navPanelIconTop} />
+          <Link to={this.props.bottomUrl} className={style.arrowBottom} />
+          <Link to={this.props.leftUrl}className={style.arrowLeft} />
+          <Link to={this.props.rightUrl} className={style.arrowRight} />
         </div>
         <div className={style.creatorToolContainer}>
           <Link to="/panel/edit" className={style.creatorTool}>
@@ -48,8 +53,17 @@ class PanelView extends Component {
 }
 
 PanelView.propTypes = {
-  /* params: PropTypes.shape({}).isRequired,
-  dispatch: PropTypes.func.isRequired,*/
+  topUrl: PropTypes.string,
+  bottomUrl: PropTypes.string,
+  leftUrl: PropTypes.string,
+  rightUrl: PropTypes.string,
+};
+
+PanelView.defaultProps = {
+  topUrl: null,
+  bottomUrl: 'panel/add',
+  leftUrl: 'panel/add',
+  rightUrl: 'panel/add',
 };
 
 export default PanelViewContainer(PanelView);
