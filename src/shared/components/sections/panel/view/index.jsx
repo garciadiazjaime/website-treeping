19838d27fb83b16e84c00ab2c3b9fa7fe67d05a5
jaime <garciadiazjaime@gmail.com>
2017-06-20 17:03:20 -0500

    improvs

--- src/shared/components/sections/panel/view/index.jsx
+++ src/shared/components/sections/panel/view/index.jsx
@@ -1,68 +1,52 @@
-import React, { Component, PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 
-import NavButton from '../../../layout/navButton';
+import Button from '../../../layout/panelButton';
 import PanelViewContainer from '../../../../containers/panel/view';
 
 import style from './style.scss';
 
-class PanelView extends Component {
+function PanelView({ panel }) {
+  return (<div className={style.comicContainer}>
+    <div className={style.panel}>
+      <img src={panel.image} alt={panel.title} />
 
-  constructor(args) {
-    super(args);
-    this.state = {
-      data: [],
-    };
-  }
+      <Button data={panel} position="top" />
+      <Button data={panel} position="right" />
+      <Button data={panel} position="bottom" />
+      <Button data={panel} position="left" />
 
-  render() {
-    return (<div className={style.comicContainer}>
-      <div className={style.panel}>
-        <img src="/images/the-leap.png" alt="The Leap" />
-
-        <NavButton data={this.props.panel.top} />
-        <NavButton data={this.props.panel.right} />
-        <NavButton data={this.props.panel.bottom} />
-        <NavButton data={this.props.panel.left} />
-        <div className={style.creatorToolContainer}>
-          <Link to="/panel/edit" className={style.creatorTool}>
-            <span className={style.editIcon} />
-          </Link>
-          <Link to="/panel/delete" className={style.creatorTool}>
-            <span className={style.deleteIcon} />
-          </Link>
-        </div>
+      <div className={style.creatorToolContainer}>
+        <Link to="/panel/edit" className={style.creatorTool}>
+          <span className={style.editIcon} />
+        </Link>
+        <Link to="/panel/delete" className={style.creatorTool}>
+          <span className={style.deleteIcon} />
+        </Link>
       </div>
-    </div>);
-  }
+    </div>
+  </div>);
 }
 
 PanelView.propTypes = {
-  panel: PropTypes.shape({
-    top: PropTypes.shape({}),
-    right: PropTypes.shape({}),
-    bottom: PropTypes.shape({}),
-    left: PropTypes.shape({}),
-  }),
+  panel: PropTypes.shape({}),
 };
 
 PanelView.defaultProps = {
   panel: {
-    top: {
-      position: 'top',
-      url: '/panel/top',
-    },
-    right: {
-      position: 'right',
-      url: null,
-    },
-    bottom: {
-      position: 'bottom',
-      url: '/panel/bottom',
-    },
-    left: {
-      position: 'left',
-      url: null,
+    _id: 'panelId',
+    image: '/images/the-leap.png',
+    title: 'The Leap',
+    children: {
+      left: {
+        _id: 'childrenLeftId',
+      },
+      top: {},
+      right: {
+        _id: 'childrenRightId',
+      },
+      bottom: {},
     },
   },
 };
